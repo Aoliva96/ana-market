88fbecd23602347b4e16e7faa114320d91ed08d0
Aster Oliva <asterthealabaster@gmail.com>
2024-04-04 08:50:36 -0700

    image path hotfix

--- controllers/api/itemRoutes.js
+++ controllers/api/itemRoutes.js
@@ -81,24 +81,4 @@ router.delete('/:id', withAuth, async (req, res) => {
   }
 });
 
-// router.delete('/:id', withAuth, async (req, res) => {
-//   try {
-//     const itemData = await Item.destroy({
-//       where: {
-//         id: req.params.id,
-//         user_id: req.session.user_id,
-//       },
-//     });
-
-//     if (!itemData) {
-//       res.status(404).json({ message: 'No item was found with this id!' });
-//       return;
-//     }
-
-//     res.status(200).json(itemData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
 module.exports = router;
